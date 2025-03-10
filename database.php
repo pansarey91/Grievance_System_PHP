06d73cbdba8ece2c631268060271560918b60c1a
YASH VIRAJ PANSARE <pansarey91@gmail.com>
2025-03-10 08:50:15 +0530

    Error Handled properly in database.php

--- database.php
+++ database.php
@@ -1,6 +1,10 @@
 <?php
-$conn = new PDO("mysql:host=localhost;dbname=complaint_sys", "root", "root");
-
-if (!$conn) {
-    die("Connection failed: " . $conn->connect_error);
+try {
+    $conn = new PDO("mysql:host=localhost;dbname=complaint_sys", "root", "root");
+    // Set PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully"; 
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
 }
+?>
